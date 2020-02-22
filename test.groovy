b19f5939b5118792aac3ae6f9519c6af91e0a524
bem92 <60887200+bem92@users.noreply.github.com>
2020-02-22 01:49:17 +0100

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -19,10 +19,7 @@ pipeline {
        
    stage('Build Docker Maven Image') {
    steps {
-      script {
-    def disk_size = sh(script: "ssh remote-server df / --output=avail | tail -1", returnStdout: true).trim() as Integer
-    println("disk_size = ${disk_size}")
-}
+       echo "test"
    }
    }
 }
